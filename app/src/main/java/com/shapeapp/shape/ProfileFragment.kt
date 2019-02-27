eb977cf0d9e4d80400cc722a01c2288d663f3b72
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-02-27 21:06:08 +0100

    Add TODO comment

--- app/src/main/java/com/shapeapp/shape/ProfileFragment.kt
+++ app/src/main/java/com/shapeapp/shape/ProfileFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+//  TODO: check and change everything
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
