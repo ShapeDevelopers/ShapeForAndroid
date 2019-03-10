09d34c765cb0867eee7ba8d6d99bb64302bace5d
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-03-10 13:37:29 +0100

    Make the dialog appear at the bottom of the screen

--- app/src/main/java/com/shapeapp/shape/dialogs/PublicSharesDialog.kt
+++ app/src/main/java/com/shapeapp/shape/dialogs/PublicSharesDialog.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.shapeapp.shape.R
@@ -24,6 +25,7 @@ class PublicSharesDialog : DialogFragment() {
         } ?: throw IllegalStateException("Activity can not be null")
 
         changeBackgroundToTransparent(createdDialog)
+        changePositionToBottom(createdDialog)
 
         return createdDialog
     }
@@ -34,5 +36,12 @@ class PublicSharesDialog : DialogFragment() {
      */
     private fun changeBackgroundToTransparent(createdDialog: AlertDialog) {
         createdDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    /**
+     * Make the [AlertDialog] appear at the bottom of the screen
+     */
+    private fun changePositionToBottom(createdDialog: AlertDialog) {
+        createdDialog.window?.attributes?.gravity = Gravity.BOTTOM
     }
 }
