8e0e18685f30390120f1529655f5a0920624ef6a
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-03-10 13:17:34 +0100

    Set background of dialog's root v. to transparent

--- app/src/main/java/com/shapeapp/shape/dialogs/PublicSharesDialog.kt
+++ app/src/main/java/com/shapeapp/shape/dialogs/PublicSharesDialog.kt
@@ -1,6 +1,8 @@
 package com.shapeapp.shape.dialogs
 
 import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
@@ -13,7 +15,7 @@ class PublicSharesDialog : DialogFragment() {
     //  TODO: include RecyclerView + Repo (?)
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return activity?.let {
+        val createdDialog = activity?.let {
             val builder = AlertDialog.Builder(it)
             builder
                 .setView(R.layout.dialog_public_shares)
@@ -27,5 +29,11 @@ class PublicSharesDialog : DialogFragment() {
 
             builder.create()
         } ?: throw IllegalStateException("Activity can not be null")
+
+        //  Set the background of the dialog's root view to transparent
+        //  See: https://stackoverflow.com/a/28937224
+        createdDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        return createdDialog
     }
 }
