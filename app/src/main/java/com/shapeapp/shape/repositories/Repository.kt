8a03e2cfbf47c3d6019aea8604c03a5ed1278a6f
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-03-16 15:08:42 +0100

    Add officialCardsData and its initialization

--- app/src/main/java/com/shapeapp/shape/repositories/Repository.kt
+++ app/src/main/java/com/shapeapp/shape/repositories/Repository.kt
@@ -1,7 +1,25 @@
 package com.shapeapp.shape.repositories
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.shapeapp.shape.mockupsmakers.TextMockups
+
 /**
  * Repository singleton
+ *
+ * "somedata" and "_somedata" are for proper encapsulation
  */
 object Repository {
+
+    //  TODO: add model, remote data source and others
+    //  see: https://developer.android.com/jetpack/docs/guide)
+
+    private val _officialCardsData = MutableLiveData<List<String>>()
+    val officialCardsData: LiveData<List<String>>
+        get() = _officialCardsData
+
+    init {
+        _officialCardsData.value = TextMockups.animals
+    }
+
 }
