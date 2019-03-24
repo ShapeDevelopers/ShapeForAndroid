ae5376428db87fe1d12a8af2db8ace3b80102619
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-03-24 16:28:07 +0100

    Add "Card" class to hold single card's data

---
+++ app/src/main/java/com/shapeapp/shape/data/Card.kt
@@ -0,0 +1,15 @@
+package com.shapeapp.shape.data
+
+/**
+ * Contains single card's data
+ */
+class Card {
+
+    var extraText = ""
+    var imageUrl = ""
+    var senderNickname = ""
+    var remainingTimeInMin = 0
+    var votesForCounter = 0
+    var votesAgainstCounter = 0
+
+}
