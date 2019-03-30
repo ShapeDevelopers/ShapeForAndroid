2bfd45f78c266f440d45645a7cd7c4b1e31168a2
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-03-31 00:15:36 +0100

    Remove unused autogenerated interface

--- app/src/main/java/com/shapeapp/shape/fragments/ReceivedImageFragment.kt
+++ app/src/main/java/com/shapeapp/shape/fragments/ReceivedImageFragment.kt
@@ -1,6 +1,5 @@
 package com.shapeapp.shape.fragments
 
-import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -30,8 +29,6 @@ private const val ARG_CARD_PARCELABLE = "ARG_CARD_PARCELABLE"
 class ReceivedImageFragment : Fragment() {
 
     private lateinit var viewModel: ReceivedImageFragmentViewModel
-
-    private var listener: OnFragmentInteractionListener? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -81,41 +78,6 @@ class ReceivedImageFragment : Fragment() {
                     against_counter_textview.text = againstCounterIndication.toString()
                 })
         }
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
     }
 
     companion object {
