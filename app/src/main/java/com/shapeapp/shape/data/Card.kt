00306623a488a68fd6a89a74f0ec7b91710bfcc8
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-03-30 23:23:02 +0100

    Implement Parcelable

--- app/src/main/java/com/shapeapp/shape/data/Card.kt
+++ app/src/main/java/com/shapeapp/shape/data/Card.kt
@@ -1,11 +1,12 @@
 package com.shapeapp.shape.data
 
+import android.os.Parcel
+import android.os.Parcelable
+
 /**
  * Contains single card's data
  */
-class Card {
-
-    //  TODO: implement [Parcelable] interface in [Card] to pass it in [Bundle]
+class Card() : Parcelable {
 
     var extraText = ""
     var imageUrl = ""
@@ -13,5 +14,37 @@ class Card {
     var remainingTimeInMin = 0
     var votesForCounter = 0
     var votesAgainstCounter = 0
+
+    constructor(parcel: Parcel) : this() {
+        extraText = parcel.readString()
+        imageUrl = parcel.readString()
+        senderNickname = parcel.readString()
+        remainingTimeInMin = parcel.readInt()
+        votesForCounter = parcel.readInt()
+        votesAgainstCounter = parcel.readInt()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(extraText)
+        parcel.writeString(imageUrl)
+        parcel.writeString(senderNickname)
+        parcel.writeInt(remainingTimeInMin)
+        parcel.writeInt(votesForCounter)
+        parcel.writeInt(votesAgainstCounter)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Card> {
+        override fun createFromParcel(parcel: Parcel): Card {
+            return Card(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Card?> {
+            return arrayOfNulls(size)
+        }
+    }
 
 }
