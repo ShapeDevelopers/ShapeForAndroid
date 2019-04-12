4c3a83a4995d0df5267cbbc011db8ace85e43b98
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-04-13 00:17:53 +0200

    Optimize imports

--- app/src/main/java/com/shapeapp/shape/fragments/MessagesFragment.kt
+++ app/src/main/java/com/shapeapp/shape/fragments/MessagesFragment.kt
@@ -1,9 +1,7 @@
 package com.shapeapp.shape.fragments
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
