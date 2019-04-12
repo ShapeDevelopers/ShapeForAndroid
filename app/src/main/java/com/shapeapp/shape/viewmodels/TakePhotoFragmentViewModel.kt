fc75b7fe5432fe6e9f6b7e5477a7ba5f06b2cff6
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-04-12 23:12:46 +0200

    Add comments

--- app/src/main/java/com/shapeapp/shape/viewmodels/TakePhotoFragmentViewModel.kt
+++ app/src/main/java/com/shapeapp/shape/viewmodels/TakePhotoFragmentViewModel.kt
@@ -15,6 +15,9 @@ class TakePhotoFragmentViewModel : ViewModel() {
     val photoUri: LiveData<Uri>
         get() = _photoUri
 
+    /**
+     * Holds [Uri] for possible photo file when View is waiting for result from camera
+     */
     private var possiblePhotoUri: Uri? = null
 
     private val _isCameraIconVisible = MutableLiveData<Boolean>()
@@ -32,14 +35,21 @@ class TakePhotoFragmentViewModel : ViewModel() {
     }
 
     /**
+     * Gets [Uri] corresponding to empty content for single view
      * See: https://developer.android.com/reference/android/widget/ImageView#setImageURI(android.net.Uri)
      */
     private fun getEmptyContentUri(): Uri? = null
 
+    /**
+     * Invoked when View starts waiting for result from camera
+     */
     fun startedWaitingForPhoto(possiblePhotoUri: Uri) {
         this.possiblePhotoUri = possiblePhotoUri
     }
 
+    /**
+     * Invoked when View gets result from camera
+     */
     fun finishedWaitingForPhoto() {
         _photoUri.value = possiblePhotoUri
         _isCameraIconVisible.value = false
