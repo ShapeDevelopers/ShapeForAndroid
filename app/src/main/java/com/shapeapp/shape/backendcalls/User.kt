15298e66673f0c7aacee60a8e70f202994dad8f1
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-04-18 17:11:51 +0200

    Add User class

---
+++ app/src/main/java/com/shapeapp/shape/backendcalls/User.kt
@@ -0,0 +1,15 @@
+package com.shapeapp.shape.backendcalls
+
+/**
+ * Represents object received from network
+ */
+class User {
+
+    val name: String = ""
+
+    val surname: String = ""
+
+    val email: String = ""
+
+    val birthDate: String = ""
+}
