24c6f8bef7865779f7e572193a52a8bd680c82f1
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-04-18 17:22:16 +0200

    Add Retrofit annotation

--- app/src/main/java/com/shapeapp/shape/backendcalls/BackendAPI.kt
+++ app/src/main/java/com/shapeapp/shape/backendcalls/BackendAPI.kt
@@ -1,8 +1,13 @@
 package com.shapeapp.shape.backendcalls
 
 import retrofit2.Call
+import retrofit2.http.GET
 
+/**
+ * Defines REST API for Retrofit
+ */
 interface BackendAPI {
 
+    @GET("user")
     fun getUser(): Call<User>
 }
