b9f3e02b464257163ef003b38ee88e94f0d61a8f
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-04-18 17:15:33 +0200

    Add annotations for object serialization

--- app/src/main/java/com/shapeapp/shape/backendcalls/User.kt
+++ app/src/main/java/com/shapeapp/shape/backendcalls/User.kt
@@ -1,15 +1,21 @@
 package com.shapeapp.shape.backendcalls
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Represents object received from network
  */
 class User {
 
+    @SerializedName("name")
     val name: String = ""
 
+    @SerializedName("surname")
     val surname: String = ""
 
+    @SerializedName("email")
     val email: String = ""
 
+    @SerializedName("dateOfBirth")
     val birthDate: String = ""
 }
