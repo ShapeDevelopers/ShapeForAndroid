ac37130636f2abdcd6f894a7dba8596f43c9e9a9
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-04-27 16:50:30 +0200

    Remove fragment initialization parameters
    If needed, use Safe Args.

--- app/src/main/java/com/shapeapp/shape/fragments/MessagesFragment.kt
+++ app/src/main/java/com/shapeapp/shape/fragments/MessagesFragment.kt
@@ -9,31 +9,12 @@ import androidx.navigation.fragment.findNavController
 import com.shapeapp.shape.R
 import kotlinx.android.synthetic.main.fragment_messages.*
 
-//  TODO: check and change everything
-
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+//  TODO: complete messages loading
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MessagesFragment.newInstance] factory method to
- * create an instance of this fragment.
  */
 class MessagesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,7 +26,6 @@ class MessagesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         configureButtons()
     }
 
@@ -57,25 +37,5 @@ class MessagesFragment : Fragment() {
 
     private fun loadProfileFragment() {
         findNavController().navigate(R.id.action_messagesFragment_to_profileFragment)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MessagesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MessagesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
