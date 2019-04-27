d6b6e8ba6427a08e6cbf664435ecd804c6141140
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-04-27 17:06:55 +0200

    Implement swipe to close with Navigation component

--- app/src/main/java/com/shapeapp/shape/fragments/ReceivedImageFragment.kt
+++ app/src/main/java/com/shapeapp/shape/fragments/ReceivedImageFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.shapeapp.shape.R
 import com.shapeapp.shape.data.Card
@@ -52,7 +53,7 @@ class ReceivedImageFragment : Fragment() {
     private fun enableSwipeToClose() {
         root_layout.setOnTouchListener(object : OnFourWaysSwipeListener(context!!) {
             override fun onSwipeBottom() {
-                activity?.run { supportFragmentManager.popBackStack() }
+                findNavController().navigate(R.id.action_receivedImageFragment_to_publicFragment)
             }
         })
     }
