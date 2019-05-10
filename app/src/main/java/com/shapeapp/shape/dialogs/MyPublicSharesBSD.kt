e744db7df400112378a74453b7fea32f11d52c5d
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-05-11 00:14:19 +0200

    Add MyPublicSharesBSD class

---
+++ app/src/main/java/com/shapeapp/shape/dialogs/MyPublicSharesBSD.kt
@@ -0,0 +1,15 @@
+package com.shapeapp.shape.dialogs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.shapeapp.shape.R
+
+class MyPublicSharesBSD : BottomSheetDialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.bsd_my_public_shares, container, false)
+    }
+}
