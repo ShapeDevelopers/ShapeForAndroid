60ae61e3ed2bdffe31817e0342417fd0c040ac0f
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-05-15 00:43:50 +0200

    Initialize Fotoapparat instance

--- app/src/main/java/com/shapeapp/shape/fragments/CameraFragment.kt
+++ app/src/main/java/com/shapeapp/shape/fragments/CameraFragment.kt
@@ -7,6 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.shapeapp.shape.R
+import io.fotoapparat.Fotoapparat
+import io.fotoapparat.parameter.ScaleType
+import kotlinx.android.synthetic.main.fragment_camera.*
 
 /**
  * A simple [Fragment] subclass.
@@ -14,12 +17,27 @@ import com.shapeapp.shape.R
  */
 class CameraFragment : Fragment() {
 
+    private lateinit var fotoapparat: Fotoapparat
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCamera()
+    }
+
+    private fun initCamera() {
+        fotoapparat = Fotoapparat(
+            context = context!!,
+            view = camera_cameraview,
+            scaleType = ScaleType.CenterCrop
+        )
     }
 
 
