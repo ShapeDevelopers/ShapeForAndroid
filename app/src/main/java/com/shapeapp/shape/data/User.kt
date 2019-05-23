88ed50f6104169e85ecd91a5237d5b48b39ed7f4
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-05-23 18:28:00 +0200

    Create User class

---
+++ app/src/main/java/com/shapeapp/shape/data/User.kt
@@ -0,0 +1,17 @@
+package com.shapeapp.shape.data
+
+/**
+ * Contains single user's data
+ */
+class User {
+
+    var nickname = ""
+    var avatarUri = ""
+    var name = ""
+    var surname = ""
+    var sex = ""
+    var email = ""
+    var birthDate = ""
+    var radarRadius = ""
+
+}
