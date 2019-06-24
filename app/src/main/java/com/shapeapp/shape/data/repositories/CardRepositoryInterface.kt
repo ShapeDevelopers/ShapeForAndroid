4aaac502214cba8da9dc161b996af646c1adc7fe
Mikolaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2019-06-24 15:54:54 +0200

    Create CardRepositoryInterface

---
+++ app/src/main/java/com/shapeapp/shape/data/repositories/CardRepositoryInterface.kt
@@ -0,0 +1,16 @@
+package com.shapeapp.shape.data.repositories
+
+import androidx.lifecycle.LiveData
+import com.shapeapp.shape.data.database.entities.Card
+
+/**
+ * Defines repository for [Card]s
+ */
+interface CardRepositoryInterface {
+
+    fun getOfficialCards(): LiveData<List<Card>>
+    fun getNewCards(): LiveData<List<Card>>
+    fun getLatestCards(): LiveData<List<Card>>
+    fun getPublicSharesCards(): LiveData<List<Card>>
+
+}
